79a7d6ca76d59ffc3d980ae2d43a887cb6a6eb96
Atom <112673913+Atom-Pch@users.noreply.github.com>
2024-03-04 21:59:10 +0700

    password hash and authorization

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,36 +10,43 @@
     <script src="script.js"></script>
     <script src="./node_modules/preline/dist/preline.js"></script>
 </head>
+
 <body class="flex">
-<?php
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "fwp_project"; // change to your db
-            $conn = mysqli_connect($servername, $username, $password, $dbname);
-            if (!$conn) {
-                die("Connection failed: " . mysqli_connect_error());
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "fwp_project"; // change to your db
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    if (isset($_POST["username"]) && isset($_POST["password"])) { // main login verification function
+        $user = $_POST['username'];
+        $pass = $_POST['password'];
+
+        $getPw = "SELECT pass FROM accounts WHERE username = '$user';";
+        $getPos = "SELECT position FROM employees JOIN accounts USING (emp_id) WHERE username = '$user';";
+        $pwResult = mysqli_query($conn, $getPw);
+        $posResult = mysqli_query( $conn, $getPos);
+
+        if ($pwResult && mysqli_num_rows($pwResult) == 1) {
+            $pwRow = mysqli_fetch_assoc($pwResult);
+            $hashed_password = $pwRow['pass'];
+            $posRow = mysqli_fetch_assoc($posResult);
+            $position = $posRow['position'];
+
+            if (password_verify($pass, $hashed_password) && $position == 'ผู้จัดการ') { // for manager
+                header("Location: main_manager.php");
+                exit();
             }
-
-            if (isset($_POST["username"]) && isset($_POST["password"])) {
-                $user = $_POST['username'];
-                $pass = $_POST['password'];
-
-                $checksql = "SELECT * FROM accounts WHERE username = '$user' AND pass = '$pass';";
-                $result = mysqli_query($conn, $checksql);
-
-                if (mysqli_num_rows($result) == 1) {
-                    $row = mysqli_fetch_assoc($result);
-                    $emp_id = $row['emp_id'];
-
-                    $fnsql = "SELECT first_name, position FROM employees WHERE emp_id = $emp_id;";
-                    $getfn = mysqli_query($conn, $fnsql);
-                    $row = mysqli_fetch_assoc($getfn);
-                    echo "<script>alert('Welcome, " . $row['position'] . " " . $row['first_name'] . "');</script>"; // delete later
-                    header("Location: main_manager.php");
-                    exit();
-                } else {
-                    echo "<div id=\"alert\" class=\"block absolute top-4 start-[16%] w-[20rem] bg-orange-100 border text-sm rounded-lg border-red-900 text-red-500\" role=\"alert\">
+            else if (password_verify($pass, $hashed_password) && $position == "พนักงาน") { // for staff
+                header("Location: main_staff.php");  // change to actual staff page
+                exit();
+            }
+        }
+        echo "<div id=\"alert\" class=\"block absolute top-4 start-[16%] w-[20rem] bg-orange-100 border text-sm rounded-lg border-red-900 text-red-500\" role=\"alert\">
                     <div class=\"flex p-4\">
                         <div>
                             <h5 class=\"font-bold text-lg\">เข้าสู่ระบบล้มเหลว</h5>
@@ -51,27 +59,31 @@
                             </button>
                         </div>
                     </div>
-                  </div>";
-                }
-            }
-            ?>
+                </div>";
+    }
+    ?>
     <div class="w-[50vw] bg-orange-300 h-[100vh]">
         <div class=" grid grid-cols-2 gap-4 w-[100%]">
-            <h1 class="text-4xl font-bold mt-3 ml-5">FWP<p class="text-2xl">: Authentication</p></h1>
+            <h1 class="text-4xl font-bold mt-3 ml-5">FWP<p class="text-2xl">: Authentication</p>
+            </h1>
             <p id="time" class="mx-4 my-3 text-right font-medium"></p>
         </div>
         <div class="mx-10 my-[20vh] bg-orange-200 rounded-lg shadow-2xl border-1 border-orange-400">
             <h2 class="text-center font-bold text-2xl pt-5 pl-5 pr-5">เข้าสู่ระบบ</h2>
             <form action="login.php" class="p-5" method="post">
                 <label for="input-label" class="block text-sm font-medium mb-2">ชื่อผู้ใช้</label>
-                <input class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-2 focus:outline-none focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" type="text" name="username" placeholder="Username" required>
+                <input
+                    class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-2 focus:outline-none focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+                    type="text" name="username" placeholder="Username" required>
                 <label for="input-label" class="block text-sm font-medium mt-4 mb-2">รหัสผ่าน</label>
-                <input class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-2 focus:outline-none focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" type="password" name="password" placeholder="Password" required>
+                <input
+                    class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-2 focus:outline-none focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+                    type="password" name="password" placeholder="Password" required>
                 <div class="mt-5 text-center">
                     <input class="prompt-medium button btn btn-primary" type="submit" value="เข้าสู่ระบบ">
                 </div>
             </form>
-        
+
         </div>
     </div>
     <div class="w-[50vw] h-[100vh]">
@@ -83,13 +95,12 @@
     var timeDisplay = document.getElementById("time");
 
     function refreshTime() {
-        var dateString = new Date().toLocaleString("en-US", {timeZone: "Asia/Bangkok"});
+        var dateString = new Date().toLocaleString("en-US", { timeZone: "Asia/Bangkok" });
         var formattedString = dateString.replace(", ", " - ");
         timeDisplay.innerHTML = formattedString;
     }
 
     setInterval(refreshTime, 1000);
 </script>
+
 </html>
-
-
